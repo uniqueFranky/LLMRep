8e0d227f40c3f3f6a60e124fe84d18c6c5f642de
uniqueFranky <y_r_b@qq.com>
2025-11-24 15:42:37 +0800

    fix: metrics

--- src/metrics.py
+++ src/metrics.py
@@ -17,6 +17,8 @@ def rep_n(tokens: Union[List[int], List[str]], n: int) -> float:
     tokens: token_id 或 token本身
     n: n-gram
     """
+    if len(tokens) < n:
+        return 0.0
     grams = [tuple(tokens[i: i + n]) for i in range(len(tokens) - n + 1)]
     unique_grams = set(grams)
     return 1 - len(unique_grams) / (len(tokens) - n + 1)
@@ -28,7 +30,7 @@ def rep_r(tokens: Union[List[int], List[str]]) -> float:
         for j in range(len(tokens)):
             if i == j:
                 continue
-            if tokens[i] == tokens[j] and ((i > 0 and tokens[i - 1] == tokens[j - 1]) or (j + 1 < len(tokens) and tokens[i + 1] == tokens[j + 1])):
+            if tokens[i] == tokens[j] and ((i > 0 and j > 0 and tokens[i - 1] == tokens[j - 1]) or (j + 1 < len(tokens) and i + 1 < len(tokens) and tokens[i + 1] == tokens[j + 1])):
                 rep += 1
                 break
     return rep / len(tokens)
